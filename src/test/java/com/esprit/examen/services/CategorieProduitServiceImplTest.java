11c8b47d96908a87eddd5f917d5822f698724e47
BenghorbelNaoures <108528814+BenghorbelNaoures@users.noreply.github.com>
2023-04-15 09:45:20 +0100

    Update CategorieProduitServiceImplTest.java

--- src/test/java/com/esprit/examen/services/CategorieProduitServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/CategorieProduitServiceImplTest.java
@@ -1,6 +1,16 @@
 package com.esprit.examen.services;
 
-/*import com.esprit.examen.repositories.CategorieProduitRepository;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.esprit.examen.entities.CategorieProduit;
+import com.esprit.examen.repositories.CategorieProduitRepository;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -10,16 +20,13 @@ class CategorieProduitServiceImplTest {
     @MockBean
     public CategorieProduitRepository repo;
     @Test
-    void contextLoads() {
-    }
     public void retrieveAllCategorieProduitsTest() {
         CategorieProduit cp = new CategorieProduit(1L,"test", "test",null);
         CategorieProduit cp2 = new CategorieProduit(2L,"test2", "test",null);
-
-        when(repo.findAll()).thenReturn(Stream
-        .of(cp,cp2)).collect(Collectors.toList());
+        service.addCategorieProduit(cp2);
+        service.addCategorieProduit(cp);
+        service.retrieveAllCategorieProduits();
         assertEquals(2, service.retrieveAllCategorieProduits().size());
-        //.of(new CategorieProduit()) categorieProduitServiceImpl.retrieveAllCategorieProduits();
     }
 
-}*/
+}
